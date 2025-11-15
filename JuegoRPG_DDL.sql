cfaa6715ba8c4e4d44ec30633d2ceeccf6f8dd3b
cgiron11 <carlosgiron524@gmail.com>
2025-11-14 18:10:54 -0600

    Nueva Tabla y correcciones de Instancias

--- JuegoRPG_DDL.sql
+++ JuegoRPG_DDL.sql
@@ -14,7 +14,6 @@ CREATE TABLE JuegoRPG.Personajes (
     NombrePersonaje NVARCHAR(50) NOT NULL UNIQUE,
     Nivel INT NOT NULL,
     Vida INT NOT NULL,
-    Inventario_ID INT NOT NULL,
     Clase_ID INT NOT NULL
 );
 
@@ -31,27 +30,36 @@ CREATE TABLE JuegoRPG.Habilidades (
 CREATE TABLE JuegoRPG.Items (
     ID INT IDENTITY(1,1) PRIMARY KEY,
     NombreItem NVARCHAR(100) NOT NULL,
-    TipoItem NVARCHAR(50) NOT NULL,
-    Descripcion NVARCHAR(500) NOT NULL,
 );
-
+;
 --Tabla de Inventarios
 CREATE TABLE JuegoRPG.Inventarios (
     ID INT IDENTITY(1,1) PRIMARY KEY,
-    Capacidad INT NOT NULL,
+    NombreInventario NVARCHAR(100)
+);
+
+--Tabla de ASIGNACION
+CREATE TABLE JuegoRPG.Asigno (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Inventario_ID INT NOT NULL,
     Item_ID INT NOT NULL,
-    Equipado BIT NOT NULL DEFAULT 0
+    Personaje_ID INT NOT NULL,
 );
 
 ---LLAVES FORANEAS---
 --Tabla de personajes
 ALTER TABLE JuegoRPG.Personajes
 ADD CONSTRAINT FK_Personajes_Clases FOREIGN KEY (Clase_ID) REFERENCES JuegoRPG.Clases(ID);
-ALTER TABLE JuegoRPG.Personajes
-ADD CONSTRAINT FK_Personajes_Inventarios FOREIGN KEY (Inventario_ID) REFERENCES JuegoRPG.Inventarios(ID);
 --Tabla de Habilidades
 ALTER TABLE JuegoRPG.Habilidades
 ADD CONSTRAINT FK_Habilidades_Clases FOREIGN KEY (Clase_ID) REFERENCES JuegoRPG.Clases(ID);
---Tabla de Inventarios
-ALTER TABLE JuegoRPG.Inventarios
-ADD CONSTRAINT FK_Inventarios_Items FOREIGN KEY (Item_ID) REFERENCES JuegoRPG.Items(ID);
+--Tabla de ASIGNACION
+ALTER TABLE JuegoRPG.Asigno
+ADD CONSTRAINT FK_Asigno_Inventarios FOREIGN KEY (Inventario_ID) REFERENCES JuegoRPG.Inventarios(ID);
+ALTER TABLE JuegoRPG.Asigno
+ADD CONSTRAINT FK_Asigno_Items FOREIGN KEY (Item_ID) REFERENCES JuegoRPG.Items(ID);
+ALTER TABLE JuegoRPG.Asigno
+ADD CONSTRAINT FK_Asigno_Personajes FOREIGN KEY (Personaje_ID) REFERENCES JuegoRPG.Personajes(ID);
+-- Constraint para evitar que el mismo personaje tenga el mismo item duplicado en el mismo inventario
+ALTER TABLE JuegoRPG.Asigno
+ADD CONSTRAINT UQ_Asigno_Inventario_Item_Personaje UNIQUE (Inventario_ID, Item_ID, Personaje_ID);
--- insert.sql
+++ insert.sql
@@ -9,14 +9,16 @@ insert into JuegoRPG.clases (NombreClase, Descripcion)
 values ('Asesino', 'Clase sigilosa y ágil, experta en ataques sorpresa');
 
 --- INSERT FOR PERSONAJES ---
-insert into JuegoRPG.personajes (NombrePersonaje, Nivel, Vida, Inventario_ID, Clase_ID)
-values ('LUFFY', 50, 100, 1, 1);
-insert into JuegoRPG.personajes (NombrePersonaje, Nivel, Vida, Inventario_ID, Clase_ID)
-values ('LIGHT', 25, 80, 2, 4);
-insert into JuegoRPG.personajes (NombrePersonaje, Nivel, Vida, Inventario_ID, Clase_ID)
-values ('GANDALF', 15, 70, 3, 2);
-insert into JuegoRPG.personajes (NombrePersonaje, Nivel, Vida, Inventario_ID, Clase_ID)
-values ('USSOP', 30, 90, 4, 3);
+insert into JuegoRPG.personajes (NombrePersonaje, Nivel, Vida, Clase_ID)
+values ('LUFFY', 50, 100, 1);
+insert into JuegoRPG.personajes (NombrePersonaje, Nivel, Vida, Clase_ID)
+values ('LIGHT', 25, 80,4);
+insert into JuegoRPG.personajes (NombrePersonaje, Nivel, Vida, Clase_ID)
+values ('GANDALF', 15, 70,2);
+insert into JuegoRPG.personajes (NombrePersonaje, Nivel, Vida, Clase_ID)
+values ('USSOP', 30, 90,3);
+INSERT into JuegoRPG.Personajes(NombrePersonaje, Nivel, Vida, Clase_ID)
+values ('ZORO', 45, 95, 4);
 
 --- INSERT FOR HABILIDADES ---
 insert into JuegoRPG.habilidades (NombreHabilidad, Clase_ID, CostoMana, Efecto)
@@ -29,21 +31,31 @@ insert into JuegoRPG.habilidades (NombreHabilidad, Clase_ID, CostoMana, Efecto)
 values ('Golpe siniestro', 4, 25, 'Ataque sorpresa que inflige critico garantizado por la espalda');
 
 --- INSERT FOR ITEMS ---
-insert into JuegoRPG.items (NombreItem, TipoItem, Descripcion)
-values ('Espada Larga', 'Arma', 'Una espada larga y afilada, ideal para combates cuerpo a cuerpo');
-insert into JuegoRPG.items (NombreItem, TipoItem, Descripcion)
-values ('Varita Mágica', 'Arma', 'Una varita que canaliza el poder mágico del usuario');
-insert into JuegoRPG.items (NombreItem, TipoItem, Descripcion)
-values ('Arco Recurvado', 'Arma', 'Un arco ligero y resistente, perfecto para ataques a distancia');
-insert into JuegoRPG.items (NombreItem, TipoItem, Descripcion)
-values ('Daga Oculta', 'Arma', 'Una daga pequeña y afilada, ideal para ataques sigilosos');
+insert into JuegoRPG.items (NombreItem)
+values ('Espada Larga');
+insert into JuegoRPG.items (NombreItem)
+values ('Varita Mágica');
+insert into JuegoRPG.items (NombreItem)
+values ('Arco Recurvado');
+insert into JuegoRPG.items (NombreItem)
+values ('Daga Oculta');
 
 --- INSERT FOR INVENTARIOS ---
-insert into JuegoRPG.inventarios (Capacidad, Item_ID, Equipado)
-values (10, 1, 1);
-insert into JuegoRPG.inventarios (Capacidad, Item_ID, Equipado)
-values (8, 2, 1);
-insert into JuegoRPG.inventarios (Capacidad, Item_ID, Equipado)
-values (12, 3, 1);
-insert into JuegoRPG.inventarios (Capacidad, Item_ID, Equipado)
-values (6, 4, 1);
+insert into JuegoRPG.inventarios (NombreInventario)
+values ('Ataques pesados');
+insert into JuegoRPG.inventarios (NombreInventario)
+values ('Magias de fuego');
+insert into JuegoRPG.inventarios (NombreInventario)
+values ('Sobreviviente del bosque');
+insert into JuegoRPG.inventarios (NombreInventario)
+values ('Equipo de sigilo');
+
+--- INSERT FOR ASIGNACION ---
+insert into JuegoRPG.asigno (Inventario_ID, Item_ID, Personaje_ID)
+values (1, 1, 5);
+insert into JuegoRPG.asigno (Inventario_ID, Item_ID, Personaje_ID)
+values (2, 2, 6);
+insert into JuegoRPG.asigno (Inventario_ID, Item_ID, Personaje_ID)
+values (3, 3, 7);
+insert into JuegoRPG.asigno (Inventario_ID, Item_ID, Personaje_ID)
+values (4, 4, 8);
